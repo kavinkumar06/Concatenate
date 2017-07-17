4b438cb451487e752290775920dfa88528b8e439
kavinkumar06 <kavinm09@gmail.com>
2017-07-17 16:42:31 +0530

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,15 @@
+# Concatenate
+import java.io.*;
+import java.util.*;
+public class Concatenate
+{
+ public static void main(String args[])
+ {
+   Scanner s=new Scanner(System.in);
+	String a=s.next();
+	String b=s.next();
+	System.out.println(a+b);
+	
+	}
+
+}
